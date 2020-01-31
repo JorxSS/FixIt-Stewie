e6744b885f7e0d36453223ee2b9c615de37f1b5c
Mario Fernández Villalba <mariobcn3@gmail.com>
2020-01-31 20:31:38 +0100

    Add GameManager

---
+++ Assets/GameManager.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    [Header("Timers")]
+    public float levelTime = 100f;
+
+    [SerializeField]
+    float currentTime = 0f;
+
+    [Header("Chores")]
+    public int numChoresToWin = 10;
+
+    [SerializeField]
+    float currentChoresDone = 0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentTime = 0f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        currentTime += Time.deltaTime;
+        if (currentTime >= levelTime)
+        {
+            Debug.Log("You lose!");
+        }
+
+        if (currentChoresDone >= numChoresToWin)
+        {
+            Debug.Log("You win!");
+        }
+    }
+}
