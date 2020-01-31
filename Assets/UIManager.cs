7afa4ea7778378aad56c426c2a90e88fd474e10f
Mario Fernández Villalba <mariobcn3@gmail.com>
2020-02-01 00:19:30 +0100

    Add UImanager

---
+++ Assets/UIManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIManager : MonoBehaviour
+{
+    [Header("GameState Windows")]
+    public GameObject winScreen;
+    public GameObject loseScreen;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void SwitchWinScreen(bool enabled)
+    {
+        winScreen.SetActive(enabled);
+    }
+
+    public void SwitchLoseScreen(bool enabled)
+    {
+        loseScreen.SetActive(enabled);
+    }
+}
