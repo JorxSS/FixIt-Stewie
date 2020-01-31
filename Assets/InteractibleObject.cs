569778b3a2feadced3514051c9a22dc30b05b76c
Enrique Alexandre González Sequeira <enrique.alexand.gonzalez@est.fib.upc.edu>
2020-01-31 20:31:22 +0100

    BaseInteractibleObject

---
+++ Assets/InteractibleObject.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InteractibleObject : MonoBehaviour
+{
+
+    enum State
+    {
+        IDLE,
+        ATTACHED,
+        DESTROYED,
+        REPAIRED
+
+    };
+
+    State state;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        state = IDLE;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
