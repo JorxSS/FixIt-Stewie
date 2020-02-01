f20444cfe77ae6180db3bd3186eac5532f8d4bc4
JorxSS <49075678+JorxSS@users.noreply.github.com>
2020-02-01 19:50:40 +0100

    Fade Canvas starts working

--- Assets/CanvasIntroController.cs
+++ Assets/CanvasIntroController.cs
@@ -6,12 +6,24 @@ using UnityEngine.UI;
 public class CanvasIntroController : MonoBehaviour
 {
     // Start is called before the first frame update
-    public GameObject house;
-    RawImage imageHouseDay;
-    bool fading;
+    public GameObject house0;
+    public GameObject house1;
+    public GameObject houseNight;
+    public GameObject dad;
+    public GameObject mom;
+    public GameObject bocata;
+    Stack<GameObject> obj;
+    RawImage currentObj;
+
+    bool fading = false;
     void Start()
     {
-        imageHouseDay = house.GetComponent<RawImage>();
+        obj.Push(house0)
+        obj.Push(dad);
+        obj.Push(mom);
+        obj.Push(bocata);
+        obj.Push(houseNight);
+   
     }
 
     // Update is called once per frame
@@ -20,16 +32,21 @@ public class CanvasIntroController : MonoBehaviour
         if (fading == true)
         {
             //Fully fade in Image (1) with the duration of 2
-            imageHouseDay.CrossFadeAlpha(1, 2.0f, false);
+            
+            imageHouseDay.CrossFadeAlpha(0, 2.0f, false);
         }
         if (fading == false)
         {
-            imageHouseDay.CrossFadeAlpha(0, 2.0f, false);
+            imageHouseDay.CrossFadeAlpha(1, 2.0f, false);
         }
     }
     void OnGUI()
     {
         //Fetch the Toggle's state
         fading = GUI.Toggle(new Rect(0, 0, 100, 30), fading, "Fade In/Out");
+    }
+    GameObject GetNext()
+    {
+        for(int)
     }
 }
