451e82820852af4bf0ed03187307ffb0852c59ca
Enrique Alexandre González Sequeira <enrique.alexand.gonzalez@est.fib.upc.edu>
2020-02-01 13:49:12 +0100

    Icons chores

---
+++ Assets/Scripts/ChoresProgres.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ChoresProgres : MonoBehaviour
+{
+
+    public Image circleContainer;
+    public Image circleDestroy;
+    public Image circleRepaired;
+
+    public int numberOfContainer = 2;
+    public int numberOfDestroy = 1;
+    public int numberRepaired = 1;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void ChoreCompleted()
+    {
+        
+    }
+}
