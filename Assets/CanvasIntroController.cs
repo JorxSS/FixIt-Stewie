8f8d434d4d4c4fb2e59c5997eb15603fd5ec3b4c
JorxSS <49075678+JorxSS@users.noreply.github.com>
2020-02-01 19:08:06 +0100

    Controller Canvas

---
+++ Assets/CanvasIntroController.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CanvasIntroController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Image imageParent1;
+    public Image imageParent2;
+    public Image imageDialogue;
+    public Image imageNoParty1;
+    public Image imageNoParty2;
+    public Image imageHouseDay;
+    public Image imageHouseNight;
+    bool fading;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (fading == true)
+        {
+            //Fully fade in Image (1) with the duration of 2
+            imageHouseDay.CrossFadeAlpha(1, 2.0f, false);
+        }
+        if (fading == false)
+        {
+            imageHouseDay.CrossFadeAlpha(0, 2.0f, false);
+        }
+    }
+    void OnGUI()
+    {
+        //Fetch the Toggle's state
+        fading = GUI.Toggle(new Rect(0, 0, 100, 30), fading, "Fade In/Out");
+    }
+}
