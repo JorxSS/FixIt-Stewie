9f6076762ffa06316103314aead417096d30cabd
JorxSS <49075678+JorxSS@users.noreply.github.com>
2020-02-01 20:15:44 +0100

    Fluent Fade

--- Assets/CanvasIntroController.cs
+++ Assets/CanvasIntroController.cs
@@ -6,48 +6,31 @@ using UnityEngine.UI;
 public class CanvasIntroController : MonoBehaviour
 {
     // Start is called before the first frame update
-    public GameObject house0;
-    public GameObject house1;
-    public GameObject houseNight;
-    public GameObject dad;
-    public GameObject mom;
-    public GameObject bocata;
-    Stack<GameObject> obj = new Stack<GameObject>();
+    public GameObject[] obj;
     RawImage currentObj = null;
-
-    void Start()
-    {
-        obj.Push(houseNight);
-        obj.Push(house1);
-        obj.Push(bocata);
-        obj.Push(dad);
-        obj.Push(mom);
-        obj.Push(house0);
-
-    }
-
-    // Update is called once per frame
+    int index = 0;
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Q)){
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
             Debug.Log("TRYINNNNNNNNNNNNNNNNN");
-            if (obj.Count > 0)
+            if (index < obj.Length)
             {
                 Debug.Log("hellowwwwwwwwwwwwwwwwwwwwwwww");
                 GameObject current = GetNext();
+                ++index;
                 currentObj = current.GetComponent<RawImage>();
-                //currentObj.CrossFadeAlpha(1, 3.0f, false);
-                Fade(currentObj);
+                currentObj.CrossFadeAlpha(0, 2.0f, false);
             }
         }
     }
     GameObject GetNext()
     {
-        return obj.Pop();
+        return obj[index];
     }
     IEnumerator Fade(RawImage go)
     {
-        go.CrossFadeAlpha(0, 2.0f, false);
+        go.CrossFadeAlpha(1, 3.0f, false);
         yield return new WaitForSeconds(.1f);
     }
 }
