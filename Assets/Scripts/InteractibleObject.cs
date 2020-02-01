c877fa27e6238327368129069bd2247a1a22f473
Juan Miguel de Haro Ruiz <juanmiguel1996@gmail.com>
2020-02-01 11:28:41 +0100

    Add progress bar in destroyable objects

--- Assets/Scripts/InteractibleObject.cs
+++ Assets/Scripts/InteractibleObject.cs
@@ -65,6 +65,10 @@ public class InteractibleObject : MonoBehaviour
     void IdleToDestroyed()
     {
         GameObject pBar = Instantiate(progressBarPrefab, canvas.transform);
+        Vector3 screen = Camera.main.WorldToScreenPoint(transform.position);
+        RectTransform rectTransform = pBar.GetComponent<RectTransform>();
+        Debug.Log("Screen is " + screen);
+        rectTransform.anchoredPosition = screen;
 
         StartCoroutine(WaitForActionDestroyable());
     }
