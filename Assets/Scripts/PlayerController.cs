e35d429ba9cabe14d48aaa90cd372e044f93c74e
Jordi <jordi@shiraz.es>
2020-02-01 08:20:51 +0100

    Solved ingame menu problems.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -26,7 +26,6 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-
         if (Input.GetMouseButtonDown(0))
         {
             Debug.Log("Hey dude I'm trying to move to this point" + Input.mousePosition);
