ea718d2c9229859a1ad05985ed1161936c44a663
Jordi <jordi@shiraz.es>
2020-02-01 17:55:58 +0100

    Enabled scene swap with trigger.

---
+++ Assets/TriggerScene.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerScene : MonoBehaviour
+{
+    public GameObject scene;
+    public GameObject parent;
+    // Start is called before the first frame update
+
+    void OnTriggerEnter(Collider other)
+    {
+        scene.SetActive(true);
+        parent.SetActive(false);
+    }
+}
